787f777d381b954b6257d1eea67e9af2db31bffe
AngelBrambila91 <malthael.91@gmail.com>
2026-01-09 20:22:53 -0600

    Added Numbers project and Vocabulary Project

---
+++ Part01/Chapter02/Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿// An unsigned integer is a positive whole number or 0
+uint naturalNumber = 23;
+
+// An integer is a negative or positive whole number or 0
+int integerNumber = -23;
+
+// A float is a single presicion floating-point number
+// The F or f suffix makes the value a float literal.
+// The suffix is required
+float realNumber = 2.3f;
+
+// A double is a double precision floating point number
+// double is the default for a number value with decimal point
+double anotherRealNumber =  2.3;
+
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+// Check the threee variables have the same value
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+
+// Output the variables in deximal
+Console.WriteLine($"{decimalNotation:N0}");
+Console.WriteLine($"{binaryNotation:N0}");
+Console.WriteLine($"{hexadecimalNotation:N0}");
+
+// Output the variables in hexadecimal
+Console.WriteLine($"{decimalNotation:X}");
+Console.WriteLine($"{binaryNotation:X}");
+Console.WriteLine($"{hexadecimalNotation:X}");
+
+// Min and max values on variables
+Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+
+unsafe
+{
+    Console.WriteLine($"Half uses {sizeof(Half)} bytes and can store numbers in the range {Half.MinValue:N0} to {Half.MaxValue:N0}.");
+}
---
+++ Part01/Chapter02/Vocabulary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection; // To use assembly , Type Name and so on
+// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// // #error version
+
+#region Three variables that store the number 2 million
+int decimalNotation = 2_000_000;
+// int binaryNotation= 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x001E_8480;
+#endregion
+
+// Adding some unused variables
+System.Data.DataSet ds = new();
+HttpClient client = new();
+
+// // Prefix @ to use a C# Keyword as a variable
+// string @class = "A Poorly named variable";
+// int @if = 5;
+
+// // Using Write Line without Console because added statically on general System.Console
+// WriteLine($"Computer named {Environment.MachineName} says \"No\"");
+
+#region Assembly stuff
+// Get the assembly that is the entry point for this app
+Assembly? myApp = Assembly.GetEntryAssembly();
+// If the previous line returned nothing then end the app
+if(myApp is null) return;
+// Loop trough the assemblies that my app references
+foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+{
+    // Load the assembly so we can read its details.
+    Assembly a = Assembly.Load(name);
+    // Declare variable to count the number of methods;
+    int methodCount = 0;
+    // Loop trough all the types in the assembly
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        // Add upo the counts of all methods
+        methodCount += t.GetMethods().Length;
+    }
+    // Output the count of types and their methods.
+    WriteLine("{0:N0} types with {1:N0} methods in {2} assembly",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount,
+    arg2: name.Name);
+}
+#endregion
